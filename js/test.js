fc0f35880f61a9f7096e1927e1929792361e8e06
HUN-Sopheak <sopheakhun030@gmail.com>
2022-12-12 17:11:26 +0700

    first commit

---
+++ js/test.js
@@ -0,0 +1,2 @@
+document.getElementById("demo").innerHTML="<h1 style='color: red;'>Hello JavaScript</h1>";
+document.getElementById("demo1").innerText="<h1 style='color: red;'>Hello JavaScript</h1>";
