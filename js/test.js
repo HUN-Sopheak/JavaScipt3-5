8041bbebac498574aa1c7340c79b0396430c2ce4
HUN-Sopheak <sopheakhun030@gmail.com>
2022-12-20 16:24:30 +0700

    20-12-22

--- js/test.js
+++ js/test.js
@@ -1,2 +1,3 @@
-document.getElementById("demo").innerHTML="<h1 style='color: red;'>Hello JavaScript</h1>";
-document.getElementById("demo1").innerText="<h1 style='color: red;'>Hello JavaScript</h1>";
+for(i=0;i<10;i++){
+    console.log(i+"test")
+}
